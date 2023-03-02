4b1d1c4431d1c7c88a621ccd25572ac85547515c
FelipeIgansi <lipeigansi@gmail.com>
2023-03-01 22:05:05 -0300

    Click Event

--- app/src/main/java/com/example/custoviagem/MainActivity.kt
+++ app/src/main/java/com/example/custoviagem/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.custoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.custoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCalculate.setOnClickListener(this)
+
     }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_calculate){
+            calculate()
+        }
+    }
+
+    private fun calculate(){
+        // Toast
+
+        Toast.makeText(this, "Clicado!", Toast.LENGTH_LONG).show()
+    }
+
 }
